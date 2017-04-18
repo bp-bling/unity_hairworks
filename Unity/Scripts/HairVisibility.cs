93d858461f75310fa5a0a46126f2dbe8ab46b364
Ben Cooley <benjcooley@hotmail.com>
2017-04-17 19:14:27 -0700

    Add Unity scripts, shaders and materials

---
+++ Unity/Scripts/HairVisibility.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTJ
+{
+    [ExecuteInEditMode]
+    public class HairVisibility : MonoBehaviour
+    {
+        UTJ.HairInstance hairInstance = null;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // called when Unity render this object
+        void OnWillRenderObject()
+        {
+            RenderHairInstances();
+        }
+
+        void OnRenderObject() 
+        {
+            EndRenderHairInstances();
+        }
+
+        private void RenderHairInstances()
+        {
+            GetHairInstance();
+            if (hairInstance)
+                hairInstance.HairRendering();
+        }
+
+        private void EndRenderHairInstances()
+        {
+            GetHairInstance();
+            if (hairInstance)
+                hairInstance.FinishedRenderingHair();
+        }
+
+        private void GetHairInstance()
+        {
+            if (hairInstance == null)
+            {
+                GameObject parent = transform.parent.gameObject;
+                if (parent)
+                {
+                    hairInstance = parent.GetComponent<UTJ.HairInstance>();
+                }
+            }
+        }
+
+    }
+}
